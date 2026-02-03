df58a020a2324af9b5f2fd9af7e5ab293cb34c5c
Mark Alldritt <alldritt@latenightsw.com>
2026-02-03 12:33:28 -0800

    Update aequery to 0.2.0

--- Formula/aequery.rb
+++ Formula/aequery.rb
@@ -1,10 +1,10 @@
 class Aequery < Formula
   desc "Query scriptable macOS applications using XPath-like expressions"
   homepage "https://github.com/alldritt/aequery"
-  url "https://github.com/alldritt/aequery/releases/download/v0.1.0/aequery-0.1.0.tar.gz"
-  sha256 "b2960d2e55dfe09d04af042d8a6492d3bee8cf09a104c17784146b41b7c0987e"
+  url "https://github.com/alldritt/aequery/releases/download/v0.2.0/aequery-0.2.0.tar.gz"
+  sha256 "4df1fa02183af9eddb633d9b540966eddc5c1611bf9e6893cfb64879e5fa46eb"
   license "MIT"
-  version "0.1.0"
+  version "0.2.0"
 
   def install
     bin.install "aequery"
