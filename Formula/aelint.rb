7e88da5febb9790056f79422ad77383738d0c4af
Mark Alldritt <alldritt@latenightsw.com>
2026-06-21 10:33:02 -0700

    Update aelint to 0.4.0

--- Formula/aelint.rb
+++ Formula/aelint.rb
@@ -1,16 +1,16 @@
 class Aelint < Formula
   desc "Validate and test a scriptable macOS application's scripting interface"
   homepage "https://github.com/alldritt/aequery"
-  url "https://github.com/alldritt/aequery/releases/download/aelint-v0.3.0/aelint-0.3.0.tar.gz"
-  sha256 "5beb31743641feb296bd84a61b0a1ced98d1752858b4cdc2d7da7f66fc8f929f"
+  url "https://github.com/alldritt/aequery/releases/download/aelint-v0.4.0/aelint-0.4.0.tar.gz"
+  sha256 "c2f658a02464e22e955b44c00ca2c86b9c60585c082f50225afa10035fa38024"
   license "MIT"
-  version "0.3.0"
+  version "0.4.0"
 
   def install
     bin.install "aelint"
   end
 
   test do
-    assert_match "0.3.0", shell_output("#{bin}/aelint --version")
+    assert_match "0.4.0", shell_output("#{bin}/aelint --version")
   end
 end
